c7191bb06455be69f8f4912c707db8f738972bc9
James Leslie <jaleslie02@gmail.com>
2018-09-09 00:29:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,3 +19,8 @@ function sayHiToGrandma(string) {
   var noanswer = "I can't hear you!"
   if (string.toLowerCase() === string) return noanswer
 }
+
+function sayHiToGrandma(string) {
+  var year answer = "YES INDEED"
+  if (string.toUpperCase() === string) return yesanswer
+}
