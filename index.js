e2922a46f1ed9586f2332631ce7e8a5ced55af76
James Leslie <jaleslie02@gmail.com>
2018-09-09 00:23:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function logWhisper(string) {
 }
 
 if (sayHiToGrandma(string).toLowerCase === sayHiToGrandma(string)) {
-  var response = "I can't hear you"
+  return = "I can't hear you"
 } else if (sayHiToGrandma(string).toUpperCase === sayHiToGrandma(string)) {
   var response = "YES INDEED"
 } else if (sayHiToGrandma(string) === "I love you, Grandma.") {
